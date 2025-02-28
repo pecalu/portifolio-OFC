e5bca3684c9d81fed3e84ae2258ce8a77006882e
pecalu <124682526+pecalu@users.noreply.github.com>
2025-02-28 17:27:19 -0300

    Add files via upload
    textos reais, imagens reais, ajustes nas imagens da pagina home e sobre mim

---
+++ portifolio-OFC-main/projetos/login-tematico/scripts/script.js
@@ -0,0 +1,5 @@
+function submit(){
+    document.getElementById('login__valorForm__email').value = ""
+    document.getElementById('login__valorForm__senha').value = ""
+    alert('Login efetuado com sucesso')
+}
